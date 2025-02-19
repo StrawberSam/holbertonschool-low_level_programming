1da731ec19a480363d21ba87b4edf5e8a930e49c
StrawberSam <s.roche53@hotmail.com>
2025-02-19 13:02:47 +0000

    P2-T6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int numberz;
+for (numberz = '0' ; numberz <= '9'; numberz++)
+putchar(numberz);
+putchar('\n');
+return (0);
+}
