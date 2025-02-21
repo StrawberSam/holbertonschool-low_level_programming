13fc9687716e6c4eb0c26d11a040f68a0699ec1e
StrawberSam <s.roche53@hotmail.com>
2025-02-21 13:30:18 +0000

    P3-T6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _abs - check the code
+ *@r: the letter we need for the number
+ * Return: absolute value.
+ */
+
+int _abs(int r)
+{
+
+	if (r < 0)
+		return (-(r));
+
+	return (r);
+}
