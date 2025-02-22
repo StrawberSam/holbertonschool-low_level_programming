46bc4d5070b30fcccd7f2abd01ce0baef4af3f7e
StrawberSam <s.roche53@hotmail.com>
2025-02-22 09:29:05 +0000

    P3-T10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - check the code
+ * @a: number to be computed
+ * @b: second number to be computed
+ * Return: the result.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
