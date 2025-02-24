1d0300450ddc4c37fe9bb0c3e012eb6ea0d48986
StrawberSam <s.roche53@hotmail.com>
2025-02-24 08:52:52 +0000

    P1-T0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - for uppercase character.
+ *@c: the character.
+ * Return: 1 if c is uppercase, otherwise is 0.
+ */
+
+int _isupper(int c)
+{
+	if (c == 'A')
+		return (1);
+
+	else
+		return (0);
+}
