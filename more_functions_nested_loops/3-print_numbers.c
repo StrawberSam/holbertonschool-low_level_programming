b72dc56f3f5f9583b62e4142dcb0d28f4fec3e37
StrawberSam <s.roche53@hotmail.com>
2025-02-24 12:50:51 +0000

    P1-T3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+		_putchar(a);
+	_putchar('\n');
+}
