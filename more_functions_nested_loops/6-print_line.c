c7130ba67344185d51ddd8c9bd450b77d47fd6c5
StrawberSam <s.roche53@hotmail.com>
2025-02-24 18:51:16 +0000

    P1-T6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ * @n: integer
+ * Return: no.
+ */
+
+void print_line(int n)
+{
+	int c;
+
+	for (c = 0; c < n; c++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
