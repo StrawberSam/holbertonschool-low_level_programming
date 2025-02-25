677604c62d4991ea95682fa101123cf17ae6955a
StrawberSam <s.roche53@hotmail.com>
2025-02-25 13:01:56 +0000

    P1-T9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,14 +12,14 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-		if (a % 5 == 0)
-		{
-			printf("Buzz");
-		}
-
 		if (a % 3 == 0)
 		{
 			printf("Fizz");
+		}
+
+		if (a % 5 == 0)
+		{
+			printf("Buzz");
 		}
 
 		if (a % 3 != 0 && a % 5 != 0)
