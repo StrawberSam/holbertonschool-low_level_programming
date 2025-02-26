ff321580eb5418d86afbc09386a498d1cca2943c
StrawberSam <s.roche53@hotmail.com>
2025-02-26 13:41:35 +0000

    P2-T2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - check the code
+ *@s: character we use
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	while (count[s] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
