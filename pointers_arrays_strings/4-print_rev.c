77dee0124c2bdabd0553cba035d995216c8ff70b
StrawberSam <s.roche53@hotmail.com>
2025-02-26 20:46:09 +0000

    P2-T4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ * @s: variable ou un pointeur point
+ * Return: chaine de caractere.
+ */
+
+void print_rev(char *s)
+{
+	int count = '0';
+
+	while (s[count] != '\0') /*trouve la longueur de la chaine*/
+	{
+		count++;
+	}
+
+	while (count >= 0) /*affiche la chaine en inverse*/
+	{
+		_putchar(s[count]);
+		count--;
+	}
+	_putchar('\n');
+}
