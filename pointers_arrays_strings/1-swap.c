de710411c5ea13c96085fe2fa0183ef2193076a2
StrawberSam <s.roche53@hotmail.com>
2025-02-26 12:09:23 +0000

    P2-T1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - check the code
+ * @a: character 1 for the two integers
+ * @b: charater 2
+ * Return: none.
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 98;
+	*b = 42;
+
+	*a = 42;
+	*b = 98;
+}
