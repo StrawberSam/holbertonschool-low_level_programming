fa5175acd008c78343ac02dd55fbe9c94000cc61
StrawberSam <s.roche53@hotmail.com>
2025-02-27 19:35:04 +0000

    P2-T7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,11 +1,24 @@
 #include "main.h"
+#include "2-strlen.c"
 
 /**
- * main - check the code
- *
+ * puts_half - check the code
+ * @str: variable
  * Return: Always 0.
  */
 
 void puts_half(char *str)
 {
+	int len = _strlen(str); /*longueur totale de la chaine*/
+	int start = len / 2; /* debut de depart pour la moitie de la chaine*/
+
+	if (len % 2 != 0) /*si la lgn est impaire*/
+		start++; /*commencer un caract apres le mid*/
+
+	while (str[start] != '\0') /*imprime caract de la snd moitie*/
+	{/*tant qu'on a pas atteint la fin de la chaine*/
+		_putchar(str[start]); /*affiche chaque caractere*/
+		start++;/*passe au caractere suivant*/
+	}
+_putchar('\n');/*saut de ligne*/
 }
