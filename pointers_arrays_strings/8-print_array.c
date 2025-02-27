21160b33f66cf5001144d15328b142e823af274d
StrawberSam <s.roche53@hotmail.com>
2025-02-27 20:50:41 +0000

    P2-T8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,12 +1,24 @@
 #include "main.h"
 
 /**
- * main - check the code for
- *
+ * print_array - check the code for
+ * @a: the array
+ * @n: number of elements of the array to be printed
  * Return: Always 0.
  */
 
 void print_array(int *a, int n)
 {
-	
+	int i; /*variable i pour se deplacer dans tableau*/
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *a++);
+
+		if (i < n - 1) /*si pas dernier element, ajoute virgule*/
+		{
+			printf(", ");
+		}
+	}
+printf("\n");
 }
