480a2ee664ea7c9c02ed81131a70e7908d176213
StrawberSam <s.roche53@hotmail.com>
2025-03-03 12:47:17 +0100

    P1-T2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - locate a character in a string
+ *@s: pointeur
+ *@c: caractère
+ * Return: Always 0.
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			{
+				return (s);
+			}
+		s++;
+	}
+	return (NULL);
+}
