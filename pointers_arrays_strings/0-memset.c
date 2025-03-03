25e55bb7087c8d35280c506748ea87097fa14e6a
StrawberSam <s.roche53@hotmail.com>
2025-03-03 11:46:15 +0100

    P1-T0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -11,7 +11,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 
-	while (n > 0) /*tant que n est diff de zero, la boucle continue à s'executer*/
+	while (n > 0) /*tant que n est > que zero, la boucle continue à s'executer*/
 	{
 		s[n - 1] = b; /*n est affecté à b*/
 		n--; /*remplit les indices du tableau en partant de la fin*/
