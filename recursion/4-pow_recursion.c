30a59ad72fd16b9c3b0b3ab82b981dea49a642cb
StrawberSam <s.roche53@hotmail.com>
2025-03-05 16:23:34 +0100

    P1-T4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_pow_recursion - calcule la valeur de x à la puissance de y.
+ *@x: valeur
+ *@y: puissance
+ *Return: value
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+	return (-1);
+	}
+
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	else
+	return (x * _pow_recursion(x, y - 1));
+
+}
