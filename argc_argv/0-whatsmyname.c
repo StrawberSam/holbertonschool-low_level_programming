9f4a0e9a5dc3bdfe0eefe670676c01a9712a0e46
StrawberSam <s.roche53@hotmail.com>
2025-03-06 14:40:37 +0100

    P3-T0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *main - program that prints its name
+ *Result: name of the prog
+ */
+
+int main (int argc, char *argv[])
+{
+	if (argc > 0)
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
