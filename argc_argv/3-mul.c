72703a7de835bbe0d9b4db2a7289b24ded4c4433
StrawberSam <s.roche53@hotmail.com>
2025-03-07 09:54:12 +0100

    P3-T3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *main - Write a program that multiplies two numbers.
+ *@argc: argument count
+ *@argv: argument vector/value
+ *Return: 1 if error, 0 for the result
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+	printf("error\n");
+	return (1);
+	}
+
+	num1 = atoi(argv[1]); /*convertis les arguments en entiers*/
+	num2 = atoi(argv[2]);
+
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
