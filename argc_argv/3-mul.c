d795609eb5ddbdd8fb7c592521009a50c5472eab
StrawberSam <s.roche53@hotmail.com>
2025-03-07 10:03:56 +0100

    P3-T3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]); /*convertis les arguments en entiers*/
-	num2 = atoi(argv[2]);
+	num2 = atoi(argv[2]); /*les fait passer de char à int*/
 
 	result = num1 * num2;
 
