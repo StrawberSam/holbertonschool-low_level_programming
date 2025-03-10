0da1ae6a3a932391f0006be7cd0cf3210b682bce
StrawberSam <s.roche53@hotmail.com>
2025-03-10 13:46:16 +0100

    changement code

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,14 +12,14 @@ char *create_array(unsigned int size, char c)
 	char *tableau = malloc(size);
 	unsigned int i;
 
-	if (size == 0)
-	{
-		return (NULL);
-	}
-
 	for (i = 0; i < size; i++)
 	{
 		tableau[i] = c;
+	}
+
+	if (size == 0)
+	{
+		return (NULL);
 	}
 
 	if (tableau == 0)
