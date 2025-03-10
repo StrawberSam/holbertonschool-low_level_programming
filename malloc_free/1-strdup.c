a2be1cd22fb291f7eec48548e35d5ac0b1195ee8
StrawberSam <s.roche53@hotmail.com>
2025-03-10 15:44:45 +0100

    ajout des commentaires

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,29 +11,29 @@ char *_strdup(char *str)
 	int count = 0, i;
 	char *str2;
 
-	if (str == NULL)
+	if (str == NULL) /*vérifier si str est null, si oui retourne null*/
 	{
 		return (NULL);
 	}
 
-	while (str[count] != '\0')
+	while (str[count] != '\0') /*boucle pour compter ma string*/
 	{
 		count++;
 	}
 
-	str2 = malloc(count + 1);
+	str2 = malloc(count + 1); /*allocation de mémoire après compter la lng*/
 
-	if (str2 == NULL)
+	if (str2 == NULL) /*si malloc fonctionne pas, retourne null*/
 	{
 		return (NULL);
 	}
 
-	for (i = 0; i < count; ++i)
+	for (i = 0; i < count; ++i) /*copie des caractères de str à str2*/
 	{
 		str2[i] = str[i];
 	}
 
-	str2[i] = '\0';
+	str2[i] = '\0'; /*copie caractère null à str2*/
 
 	return (str2);
 }
