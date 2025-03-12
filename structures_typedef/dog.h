6860fe7c529559955d58946d2148be7b19659588
StrawberSam <s.roche53@hotmail.com>
2025-03-12 15:29:32 +0100

    update du code

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,9 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
 #include "main.h"
 
+/**
+ *struct dog - structure for combine data items of different kinds
+ *@name: nom
+ *@age: age
+ *@owner: propriétaire
+ *
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
+#endif
