bc2d056284d57717d2284881eb479034600da91e
StrawberSam <s.roche53@hotmail.com>
2025-03-13 18:50:02 +0100

    tache 2 projet 3

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *print_dog - utiliser printf pour print
+ *@d: pointeur qui désigne ma structure
+ *Return: aucun
+ */
+
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
