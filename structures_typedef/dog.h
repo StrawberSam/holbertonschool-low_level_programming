b020d48bcb2bc1b5989057af1ddbfe0418896a5b
StrawberSam <s.roche53@hotmail.com>
2025-03-13 20:19:45 +0100

    update

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,11 +15,6 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
-/**
- * dog_t - typedef for dog
- */
-/*typedef struct dog dog_t;*/
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /* DOG_H */
