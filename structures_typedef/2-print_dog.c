5a546565203e8a69834cb51f6d8bf3c3a04b0eb8
StrawberSam <s.roche53@hotmail.com>
2025-03-13 19:30:45 +0100

    update boucle if

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,10 +13,28 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 	printf("(nil)\n");
 
-	if (d)
+	if (d != NULL)
 	{
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+		if (d->name != NULL)
+		{
+			printf("Name: %s\n", d->name);
+		}
+
+		else
+		{
+			printf("nil\n");
+		}
+		printf("Age: %f\n", d->age);
+
+		if (d->owner != NULL)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+
+		else
+		{
+			printf("nil\n");
+		}
+
 	}
 }
