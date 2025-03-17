579e5a14e481a1cd5a77b4690264fe85203f72eb
StrawberSam <s.roche53@hotmail.com>
2025-03-17 15:33:07 +0100

    maj de la boucle if en ajoutant la condition avec le tableau

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -5,23 +5,23 @@
  *@array: tableau
  *@size: taille
  *@cmp: pointeur qui pointe une fonction
- *Return: un entier
+ *Return: l'élement du tableau
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 || cmp == NULL)
+	if (size <= 0 || cmp == NULL || array == NULL)
 	{
 		return (-1);
 	}
 
 	for (i = 0; i < size; i++)/*parcours les élements du tableau*/
 	{
-		if (cmp(array[i]) != 0)/*pour chaque élements, appeler la fonction cmp*/
-		{ /*vérifie le résultat*/
-			return (i);
+		if (cmp(array[i]) != 0)/*appeler la fonction cmp + vérif*/
+		{
+			return (i);/*retourne i si c'est trouvé*/
 		}
 	}
 	return (-1);/*retourne -1 si rien n'a été trouvé*/
