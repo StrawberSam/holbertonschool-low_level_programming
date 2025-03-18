8926bd14b6f725df77dee00b8d7ceabea5bb9577
StrawberSam <s.roche53@hotmail.com>
2025-03-18 16:00:59 +0100

    Fix commentaire avec return et espace après les return

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -4,31 +4,31 @@
  *op_add - addition de a avec b
  *@a: variable
  *@b: variable
- Return: the result of the op
+ *Return: the result of the op
  */
 
 int op_add(int a, int b)
 {
-	return(a + b);
+	return (a + b);
 }
 
 /**
  *op_sub - soustraction de a avec b
  *@a: variable
  *@b: variable
- Return: the result of the op
+ *Return: the result of the op
  */
 
 int op_sub(int a, int b)
 {
-	return(a - b);
+	return (a - b);
 }
 
 /**
  *op_mul - multiplication de a avec b
  *@a: variable
  *@b: variable
- Return: the result of the op
+ *Return: the result of the op
  */
 
 int op_mul(int a, int b)
@@ -40,7 +40,7 @@ int op_mul(int a, int b)
  *op_div - division de a avec b
  *@a: variable
  *@b: variable
- Return: the result of the op
+ *Return: the result of the op
  */
 
 int op_div(int a, int b)
@@ -52,7 +52,7 @@ int op_div(int a, int b)
  *op_mod - reste de la division de a avec b
  *@a: variable
  *@b: variable
- Return: the result of the op
+ *Return: the result of the op
  */
 
 int op_mod(int a, int b)
