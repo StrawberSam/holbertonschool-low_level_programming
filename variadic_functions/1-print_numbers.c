7bd23dd5e2552f003cdef03d094b518ba084a2bd
StrawberSam <s.roche53@hotmail.com>
2025-03-19 14:00:37 +0100

    Tâche 1 du projet 2

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ *print_numbers - fonction qui print les nombres
+ *
+ * @separator: string to be printed between numbers
+ * @n: nombers d'entiers qui passent dans la fonction
+ * Return: Numbers
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	if (!separator)
+		return;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i > 0 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	printf("%d", va_arg(args, unsigned int));
+	}
+
+	va_end(args);
+	printf("\n");
+}
