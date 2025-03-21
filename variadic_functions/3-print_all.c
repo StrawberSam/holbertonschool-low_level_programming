94a470abc22f7f823165dd581108133fc0a1ce66
StrawberSam <s.roche53@hotmail.com>
2025-03-21 18:34:13 +0100

    Fix all the code

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,59 +1,78 @@
 #include "variadic_functions.h"
-
 /**
- * print_all - check the code
- * @format: Contient les diff types
- * Return: Always 0.
+ * print_char - affiche un caractère
+ * @arguments: liste d'arguments variadiques
+ */
+void print_char(va_list arguments)
+{
+    /* les char deviennent des int dans les fonctions variadiques */
+    printf("%c", va_arg(arguments, int));
+}
+/**
+ * print_int - affiche un entier
+ * @arguments: liste d'arguments variadiques
+ */
+void print_int(va_list arguments)
+{
+    /* récupère et affiche un int */
+    printf("%d", va_arg(arguments, int));
+}
+/**
+ * print_float - affiche un float (en fait un double ici)
+ * @arguments: liste d'arguments variadiques
+ */
+void print_float(va_list arguments)
+{
+    /* les floats deviennent des doubles dans les fonctions variadiques */
+    printf("%f", va_arg(arguments, double));
+}
+/**
+ * print_string - affiche une chaîne de caractères
+ * @arguments: liste d'arguments variadiques
+ */
+void print_string(va_list arguments)
+{
+    /* récupère une string (char *) */
+    char *str = va_arg(arguments, char *);
+    /* si la string est NULL, on affiche (nil) */
+    if (!str)
+        str = "(nil)";
+    printf("%s", str);
+}
+/**
+ * print_all - fonction qui affiche tout, selon les types passés dans "format"
+ * @format: chaîne de caractères indiquant les types des arguments
  */
 void print_all(const char * const format, ...)
 {
-	char *s;
-	float f;
-	int i, k = 0, j = 0, c; /* compteur, integer */
-	va_list argument;
-
-	va_start(argument, format);
-	while (format[j] != '\0')
-	{
-		if (format[j] == 'c')
-		{
-			c = va_arg(argument, int);
-			printf("%c", c);
-			printf(", ");
-		}
-		else if (format[j] == 'i')
-		{
-			i = va_arg(argument, int);
-			printf("%d", i);
-			printf(", ");
-		}
-		else if (format[j] == 'f')
-		{
-			f = va_arg(argument, double);
-			printf("%f", f);
-			printf(", ");
-		}
-		else if (format[j] == 's')
-		{
-			s = va_arg(argument, char*);
-			if (s[k] != '\0' && s != NULL)
-				printf("%s", s);
-
-			else if (s == NULL)
-				s = "(nil)";
-		}
-		k++;
-		j++;
-	}
-	printf("\n");
-	va_end(argument);
+    va_list arguments;
+    int i = 0,
+    j;
+    char *sep = "";
+    /* tableau associant types et fonctions de print */
+    type_handler handlers[] = {
+        {'c', print_char},
+        {'i', print_int},
+        {'f', print_float},
+        {'s', print_string}
+    };
+    va_start(arguments, format); /* initialisation de la liste variadique */
+    while (format && format[i]) /* on parcourt la chaîne "format" */
+    {
+        j = 0;
+        while (j < 4) /* on cherche si le type est reconnu */
+        {
+            if (format[i] == handlers[j].symbol)
+            {
+                printf("%s", sep); /* on affiche le séparateur (rien au début) */
+                handlers[j].print_func(arguments); /* on appelle la bonne fonction */
+                sep = ", "; /* à partir du 2e élément, on met une virgule */
+                break; /* on sort du while une fois le type trouvé */
+            }
+            j++;
+        }
+        i++; /* on passe au caractère suivant dans "format" */
+    }
+    va_end(arguments); /* on nettoie la liste variadique */
+    printf("\n"); /* on ajoute le saut de ligne final */
 }
-
-
-
-
-
-
-
-
-
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,13 +1,22 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-
+#include <string.h>
+/**
+ * struct type_handler - Structure qui associe un caractère de type
+ * à une fonction capable d'afficher cet argument
+ * @symbol: le caractère représentant le type (ex: 'c', 'i', 'f', 's')
+ * @print_func: pointeur vers la fonction qui affiche ce type
+ */
+typedef struct type_handler
+{
+    char symbol;
+    void (*print_func)(va_list arguments);
+} type_handler;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
 #endif /* VARIADIC_FUNCTIONS_H */
