13a32ae71d0829c711133a4f21457e35328d5797
StrawberSam <s.roche53@hotmail.com>
2025-04-01 10:59:58 +0200

    Ajout de commentaires pour la lisibiliter et compréhension du code

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -7,23 +7,24 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	size_t i = 0;/*Compteur du nombre d'éléments dans la liste*/
 
+/*Parcours de la liste jusqu'à ce que h soit NULL (fin de la liste)*/
 	while (h != NULL)
 	{
-		if (h->str == NULL)
+		if (h->str == NULL)/*Vérifie si la chaîne de caractères est NULL*/
 		{
-			printf("[0] (nil)\n");
+			printf("[0] (nil)\n");/*Affiche [0] (nil) pour un nœud sans texte*/
 		}
 
 		else
 		{
-		printf("[%u] %s\n", h->len, h->str);
+		printf("[%u] %s\n", h->len, h->str);/*Affiche [longueur] texte*/
 		}
 
-	i++;
-	h = h->next;
+	i++;/*Incrémente le compteur d'éléments*/
+	h = h->next;/*Passe au noeud suivant*/
 	}
 
-	return (i);
+	return (i);/*retourne le nombre total d'élèments parcourus*/
 }
