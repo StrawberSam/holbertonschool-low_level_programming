23bed55f091a01c82758e30478706d992efc8f12
StrawberSam <s.roche53@hotmail.com>
2025-04-03 14:05:15 +0200

    Fix code and comments for betty

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,7 +1,6 @@
 #include "lists.h"
 /**
- *free_dlistint -
- *Fonction permettant de libérer toute la mémoire
+ *free_dlistint - Fonction permettant de libérer toute la mémoire
  *occupée par une liste doublement chaînée.
  *@head: Pointeur vers le premier nœud de la liste.
  *Return: Aucun retour (void). La mémoire est libérée.
@@ -10,14 +9,14 @@
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *nextnoeud;
-
-	while (head != NULL)/*boucle pour itérer sur tous les noeuds*/
-		{
-			/*sauvegarder le noeud suivant*/
-			nextnoeud = head->next;
-			/*libération du noeud actuel*/
-			free(head);
-			/*on passe au noeud suivant*/
-			head = nextnoeud;
-		}
+	/*boucle pour itérer sur tous les noeuds*/
+	while (head != NULL)
+	{
+		/*sauvegarder le noeud suivant*/
+		nextnoeud = head->next;
+		/*libération du noeud actuel*/
+		free(head);
+		/*on passe au noeud suivant*/
+		head = nextnoeud;
+	}
 }
