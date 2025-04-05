45d332d5b5d19323d41c133862c0b1bcdcedf31e
StrawberSam <s.roche53@hotmail.com>
2025-04-05 18:06:51 +0200

    fix code si l'écriture a échoué

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -33,19 +33,21 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)/*vérification nom de fichier*/
 		return (-1);
 
-	fichier = open(filename, O_WRONLY | O_CREAT, 0600);
+	fichier = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fichier == -1)/*vérification ouverture de fichier*/
 		return (-1);
 
 
 	if (text_content != NULL)/*si c'est pas null, écrire dedans*/
+	{
 		bytes_written = write(fichier, text_content, _strlen(text_content));
 
-	if (bytes_written == -1)/*si l'écriture a échoué*/
-	{
-		close(fichier);
-		return (-1);
+		if (bytes_written == -1)/*si l'écriture a échoué*/
+		{
+			close(fichier);
+			return (-1);
+		}
 	}
 
 	close(fichier);
