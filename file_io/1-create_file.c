3151ecfad935620a1122f53de1081ec30e1155ef
StrawberSam <s.roche53@hotmail.com>
2025-04-05 17:59:22 +0200

    ajout commentaire pour betty style

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * create_file - Crée un fichier et y écrit du contenu, ou le crée vide.
+ * @filename: Nom du fichier à créer.
+ * @text_content: Contenu texte à écrire dans fichier (ou NULL pour vide).
+ * Return: 1 si succès, -1 si erreur.
+ */
+
 int create_file(const char *filename, char *text_content)
 {
 	int fichier;
